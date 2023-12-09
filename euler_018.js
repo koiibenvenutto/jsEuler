d6a6a6464da5e777d50492e1c6c595c49ec11e84
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2023-12-09 00:50:13 -0800

    starting a new branch to solve pe18 with dp

--- euler_018.js
+++ euler_018.js
@@ -17,25 +17,62 @@ const bigPyramid = [
   [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
 ];
 // First I'm going to start by making a function that can just walk through the pyramid using adjacent numbers. Then I'll add logic to make it choose the highest one. If that doesn't work, then I'll have it look a little into the future as well :)
-function greatestPath(pyramid) {
-  // Declaring a variable to keep track of the paths sum as it walks from top to bottom. We'll initialize it as the value we're starting from because we know for certain that it will be part of the sum no matter what
-  let sum = pyramid[0][0];
-  console.log(sum);
-  //   This array keeps track of the adjacent numbers which the path can choose from
-  let xPos = 0;
 
-  //   y Pos is the index for the pyramid row AKA position on the y axis
-  for (let yPos = 0; yPos < pyramid.length - 1; yPos++) {
-    if (pyramid[yPos + 1][xPos] <= pyramid[yPos + 1][xPos + 1]) {
-      sum += pyramid[yPos + 1][xPos + 1];
-      // We need to update where we are on the x pos which is a little tricky to think about because every row appears to be backsliding so to speak on the x axis in relation to the row above it, which gives it the pyramid shape. In practice though it's simple because it's just like a slanted grid where every row is one longer than the one above it :D
-      xPos++;
-    } else {
-      sum += pyramid[yPos + 1][xPos];
-    }
-    // Now we need to update the adjacent options variable so that it's corrent for the next iteration of the loop, but it depends on which adjacent option was chosen
-  }
+// First attempt 111323_1102:
+// function greatestPath(pyramid) {
+//   // Declaring a variable to keep track of the paths sum as it walks from top to bottom. We'll initialize it as the value we're starting from because we know for certain that it will be part of the sum no matter what
+//   let sum = pyramid[0][0];
+//   console.log(sum);
+//   //   This array keeps track of the adjacent numbers which the path can choose from
+//   let xPos = 0;
+
+//   //   y Pos is the index for the pyramid row AKA position on the y axis
+//   for (let yPos = 0; yPos < pyramid.length - 1; yPos++) {
+//     if (pyramid[yPos + 1][xPos] <= pyramid[yPos + 1][xPos + 1]) {
+//       sum += pyramid[yPos + 1][xPos + 1];
+//       // We need to update where we are on the x pos which is a little tricky to think about because every row appears to be backsliding so to speak on the x axis in relation to the row above it, which gives it the pyramid shape. In practice though it's simple because it's just like a slanted grid where every row is one longer than the one above it :D
+//       xPos++;
+//     } else {
+//       sum += pyramid[yPos + 1][xPos];
+//     }
+//   }
+//   return sum;
+// }
+
+// console.log(greatestPath(littlePyramid));
+
+// Second Attempt:
+// the first attempt was good but it was too short sited to be certain of finding the largest sum. Sorry Kilton but you were wrong!
+// As I suspected, we need to be a little smarter.
+// Two ideas pop into my head:
+// 1: look further ahead before commiting to a path
+// 2: Map out the highest number in every row and build a path that hits as many of them as possible along the way
+
+// I'm going to start by just having it look one row further ahead and see if that makes the difference :)
+function greatestPathV2(pyramid) {
+  let sum = pyramid[0][0];
+  let x = 0;
+
+  for (let y = 0; y < pyramid.length - 1; y++) {}
   return sum;
 }
 
-console.log(greatestPath(littlePyramid));
+function eyes(pyramid, x, y, distance = 0, sum = 0) {
+  // Base case: this determines how far ahead the function will look
+  if (distance > 1) {
+    return [sum, endingXY];
+  }
+
+  distance++;
+
+  eyes(pyramid, x + 1, y + 1, distance, (sum += pyramid[y + 1][x + 1]));
+  eyes(pyramid, x, y + 1, distance, (sum += pyramid[y + 1][x + 1]));
+
+  return [sum, endingXY];
+}
+
+const options = [eyes(littlePyramid, 0, 0)];
+
+console.log(options);
+
+// Okay so this recursive part is working, it's just formatted in a weird way because it's a bunch of arrays inside arrays
