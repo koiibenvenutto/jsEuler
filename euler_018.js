63480ac3e768d266e94119a14e56fc808a771c47
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2023-12-10 01:02:07 -0800

    Solved pe18 YAYAYAYA

--- euler_018.js
+++ euler_018.js
@@ -1,5 +1,5 @@
-const littlePyramid = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]];
-const bigPyramid = [
+const littleTriangle = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]];
+const bigTriangle = [
   [75],
   [95, 64],
   [17, 47, 82],
@@ -16,63 +16,21 @@ const bigPyramid = [
   [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
   [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
 ];
-// First I'm going to start by making a function that can just walk through the pyramid using adjacent numbers. Then I'll add logic to make it choose the highest one. If that doesn't work, then I'll have it look a little into the future as well :)
 
-// First attempt 111323_1102:
-// function greatestPath(pyramid) {
-//   // Declaring a variable to keep track of the paths sum as it walks from top to bottom. We'll initialize it as the value we're starting from because we know for certain that it will be part of the sum no matter what
-//   let sum = pyramid[0][0];
-//   console.log(sum);
-//   //   This array keeps track of the adjacent numbers which the path can choose from
-//   let xPos = 0;
-
-//   //   y Pos is the index for the pyramid row AKA position on the y axis
-//   for (let yPos = 0; yPos < pyramid.length - 1; yPos++) {
-//     if (pyramid[yPos + 1][xPos] <= pyramid[yPos + 1][xPos + 1]) {
-//       sum += pyramid[yPos + 1][xPos + 1];
-//       // We need to update where we are on the x pos which is a little tricky to think about because every row appears to be backsliding so to speak on the x axis in relation to the row above it, which gives it the pyramid shape. In practice though it's simple because it's just like a slanted grid where every row is one longer than the one above it :D
-//       xPos++;
-//     } else {
-//       sum += pyramid[yPos + 1][xPos];
-//     }
-//   }
-//   return sum;
-// }
-
-// console.log(greatestPath(littlePyramid));
-
-// Second Attempt:
-// the first attempt was good but it was too short sited to be certain of finding the largest sum. Sorry Kilton but you were wrong!
-// As I suspected, we need to be a little smarter.
-// Two ideas pop into my head:
-// 1: look further ahead before commiting to a path
-// 2: Map out the highest number in every row and build a path that hits as many of them as possible along the way
-
-// I'm going to start by just having it look one row further ahead and see if that makes the difference :)
-function greatestPathV2(pyramid) {
-  let sum = pyramid[0][0];
-  let x = 0;
-
-  for (let y = 0; y < pyramid.length - 1; y++) {}
-  return sum;
-}
-
-function eyes(pyramid, x, y, distance = 0, sum = 0) {
-  // Base case: this determines how far ahead the function will look
-  if (distance > 1) {
-    return [sum, endingXY];
+function maxPath(triangle) {
+  for (let i = triangle.length - 2; i >= 0; i--) {
+    console.log(`i: ${i}`);
+    for (let j = 0; j < triangle[i].length; j++) {
+      console.log(`j: ${j}`);
+      triangle[i][j] =
+        triangle[i][j] + Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+      // console.log(triangle);
+    }
   }
-
-  distance++;
-
-  eyes(pyramid, x + 1, y + 1, distance, (sum += pyramid[y + 1][x + 1]));
-  eyes(pyramid, x, y + 1, distance, (sum += pyramid[y + 1][x + 1]));
-
-  return [sum, endingXY];
+  console.log(triangle);
+  console.log(triangle[0][0]);
 }
 
-const options = [eyes(littlePyramid, 0, 0)];
+maxPath(bigTriangle);
 
-console.log(options);
-
-// Okay so this recursive part is working, it's just formatted in a weird way because it's a bunch of arrays inside arrays
+// What about filling in a triangle of blanks with the sums I find.
