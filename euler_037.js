a7d720f5608daab5e2275b4828b41bb5b4f5fb2e
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2024-02-08 14:58:51 -0800

    solved 37

---
+++ euler_037.js
@@ -0,0 +1,53 @@
+// From 035:
+
+const isPrime = require("./utils/isPrime");
+
+// need to remake the numRotator...into a num truncator...
+const numTruncator = (n, fromRight = true) => {
+  if (fromRight) {
+    return Number(n.toString().slice(0, n.toString().length - 1));
+  } else {
+    return Number(n.toString().slice(1));
+  }
+};
+
+// //console.log(numRotator(123));
+
+function truncPrimes(n) {
+  const truncatablePrimes = new Set();
+  for (let i = 11; i < n; i++) {
+    let num = i;
+    let j = 0;
+    let truncs = [];
+
+    let fromRight = true;
+    while (j < i.toString().length * 2) {
+      if (j === i.toString().length) {
+        num = i;
+        fromRight = false;
+      }
+      //console.log(j);
+      if (isPrime(num)) {
+        truncs.push(num);
+        //console.log(truncs);
+        num = numTruncator(num, fromRight);
+        //console.log(num);
+      } else {
+        break;
+      }
+      j++;
+    }
+
+    if (j === i.toString().length * 2) {
+      truncatablePrimes.add(...truncs);
+    }
+  }
+  return [...truncatablePrimes].reduce(
+    (accumulator, currentValue) => currentValue + accumulator,
+    0
+  );
+
+  //   return [...truncatablePrimes];
+}
+
+console.log(truncPrimes(10_000_000));
