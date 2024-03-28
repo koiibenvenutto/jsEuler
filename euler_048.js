640d33253c0f5389a5db3b2657f7fdf778dcf17f
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2024-03-28 10:04:26 -0700

    solved 48

--- euler_048.js
+++ euler_048.js
@@ -1,13 +1,15 @@
 function selfPowers(limit) {
+  limit = BigInt(limit);
+  console.log(limit);
   let power,
-    sum = 0;
-  for (let i = 1; i < limit; i++) {
+    sum = 0n;
+  for (let i = 1n; i < limit; i++) {
     console.log(i);
     power = i ** i;
     sum += power;
   }
-
-  return sum;
+  let string = sum.toString();
+  return string.slice(string.length - 10);
 }
 
-console.log(selfPowers(11));
+console.log(selfPowers(1001));
