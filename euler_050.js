18c4b8e2214a0fc4959b39eb5686cc7db2a164b9
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2024-04-19 00:07:09 -0700

    solved problem 50 I can't believe I've made it this far

--- euler_050.js
+++ euler_050.js
@@ -3,28 +3,38 @@ const isPrime = require("./utils/isPrime");
 
 function consecutivePrimeSum(limit) {
   const primes = primeSieve(limit);
-  console.log(primes);
-  let highest = 0;
-  for (let i = 1; primes[i] < limit; i++) {
-    console.log(`i: ${i}`);
-    for (let j = 0; j < i; j++) {
-      let primeSlice = primes.slice(j, i);
-      console.log(primeSlice);
-      let currentSum = primeSlice.reduce(
+  // console.log(primes);
+  // console.log(`--------`);
+  let longestSequence = 0;
+  let result;
+  for (let i = 1; i <= primes.length; i++) {
+    // console.log(`i: ${i}`);
+    for (let j = 0; j < i - longestSequence; j++) {
+      // console.log(`j: ${j}`);
+      let primesSlice = primes.slice(j, i);
+      // console.log(`primesSlice: ${primesSlice}`);
+
+      let currentSum = primesSlice.reduce(
         (currentValue, accumulator) => accumulator + currentValue,
         0
       );
+      if (currentSum > limit) break;
+      // console.log(currentSum);
 
-      console.log(currentSum);
-
-      if (isPrime(currentSum) && currentSum > highest) highest = currentSum;
+      if (isPrime(currentSum) && primesSlice.length > longestSequence) {
+        longestSequence = primesSlice.length;
+        result = currentSum;
+        // console.log(isPrime(currentSum));
+        // console.log(longestSequence);
+      }
     }
+    // console.log(`--------`);
   }
 
-  return highest;
+  return result;
 }
 
-console.log(consecutivePrimeSum(10));
+console.log(consecutivePrimeSum(1_000_000));
 
 // Okay so the issue here is that they don't have to start at 1, it can be any string of consequtive primes.
 
--- utils/sieveOfEratosthenes.js
+++ utils/sieveOfEratosthenes.js
@@ -1,12 +1,13 @@
 function sieveOfEratosthenes(n) {
   const table = Array(n + 1).fill(true);
   // console.log(table);
-  table[0] = 0;
+  table[0] = false;
   for (let i = 2; i < Math.sqrt(n); i++) {
     for (let j = i; j * i <= n; j++) {
       table[i * j] = false;
     }
   }
+  table[1] = false;
 
   const result = table.reduce(
     (acc, val, index) => (val ? acc.concat(index) : acc),
